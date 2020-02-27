ac75b1af611859d4a90f021d6018bdf56fa74009
QponahuijiuY <1530201142@qq.com>
2020-02-27 10:59:42 +0800

    add ArrayStack

--- src/com/mutong/hot_interview/ArrayStack.java
+++ src/com/mutong/hot_interview/ArrayStack.java
@@ -1,10 +1,41 @@
 package com.mutong.hot_interview;
 
 /**
- * @description:
+ * @description: 用数组实现栈
  * @Author: Mutong
  * @Date: 2020-02-27 10:03
  * @time_complexity: O()
  */
 public class ArrayStack {
+    private Integer[] arr;
+    private Integer index;//index表示的含义是我要把数字放在下标为index里面
+
+    public ArrayStack(int initSize){
+        if (initSize < 0){
+            throw new IllegalArgumentException("the init size is less than 0");
+        }
+        arr = new Integer[initSize];
+        index = 0;
+    }
+    public Integer peek(){
+        if (index == 0){
+            return null;
+        }
+        return arr[index - 1];
+
+    }
+    public void push(int n){
+        if (index == arr.length){
+            throw new ArrayIndexOutOfBoundsException("the queue is full");
+        }
+        arr[index++] = n;
+    }
+    public Integer pop(){
+        if (index == 0){
+            throw new ArrayIndexOutOfBoundsException("the queue is empty");
+        }
+        return arr[--index];
+    }
+
+
 }
