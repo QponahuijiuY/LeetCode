455f238b0c0d11b40a9cfe37a15f397f8d017be6
QponahuijiuY <1530201142@qq.com>
2020-02-28 11:52:37 +0800

    other

--- src/com/mutong/bishi/Main.java
+++ src/com/mutong/bishi/Main.java
@@ -1,5 +1,7 @@
 package com.mutong.bishi;
 
+import java.util.Scanner;
+
 /**
  * @description:
  * @Author: Mutong
@@ -7,4 +9,12 @@ package com.mutong.bishi;
  * @time_complexity: O()
  */
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = sc.nextInt();
+        if (i < n){
+
+        }
+    }
 }
