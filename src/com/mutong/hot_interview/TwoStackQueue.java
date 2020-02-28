15872fbbe39264fffa3cdc6c46fe3feda3eb7413
QponahuijiuY <1530201142@qq.com>
2020-02-28 11:51:49 +0800

    TwoStackQueue

--- src/com/mutong/hot_interview/TwoStackQueue.java
+++ src/com/mutong/hot_interview/TwoStackQueue.java
@@ -1,10 +1,53 @@
 package com.mutong.hot_interview;
 
+import java.util.Stack;
+
 /**
- * @description:
+ * @description: 用两个栈实现队列
  * @Author: Mutong
  * @Date: 2020-02-27 11:56
  * @time_complexity: O()
  */
 public class TwoStackQueue {
+    private Stack<Integer> data;
+    private Stack<Integer> help;
+
+    public TwoStackQueue(){
+        data = new Stack<>();
+        help = new Stack<>();
+    }
+
+    //入队列
+    public void add(int n){
+        data.push(n);
+        dao();
+    }
+    //查看队首元素
+    public int peek(){
+        if (data.empty() && help.empty()){
+            throw new RuntimeException("");
+        }
+        dao();
+        return help.peek();
+    }
+
+    //出队列
+    public int poll(){
+        if (data.empty() && help.empty()){
+            throw new RuntimeException("");
+        }
+        dao();
+        return help.pop();
+    }
+
+    private void dao() {
+        if (!help.isEmpty()){
+            return;
+        }
+        if (!data.isEmpty()){
+            help.push(data.pop());
+        }
+    }
+
+
 }
