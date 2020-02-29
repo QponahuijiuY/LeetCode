5dc868351366ec8f45d4ccfc11eba429bee6ae27
QponahuijiuY <1530201142@qq.com>
2020-02-29 23:10:23 +0800

    树结构

--- src/com/mutong/hot_interview/TreeNode.java
+++ src/com/mutong/hot_interview/TreeNode.java
@@ -1,10 +1,17 @@
 package com.mutong.hot_interview;
 
 /**
- * @description:
+ * @description: 树结构
  * @Author: Mutong
  * @Date: 2020-02-29 23:01
  * @time_complexity: O()
  */
 public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
 }
