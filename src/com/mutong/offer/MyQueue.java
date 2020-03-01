65cac63ebbd992f557032bf5a03142fc9d60784e
QponahuijiuY <1530201142@qq.com>
2020-03-01 22:01:58 +0800

    MyStack

--- src/com/mutong/offer/MyQueue.java
+++ src/com/mutong/offer/MyQueue.java
@@ -1,10 +1,49 @@
 package com.mutong.offer;
 
+import java.util.Stack;
+
 /**
- * @description:
+ * @description: 用栈实现队列
  * @Author: Mutong
  * @Date: 2020-03-01 21:11
  * @time_complexity: O()
  */
 public class MyQueue {
+    Stack<Integer> stack;
+    Stack<Integer> help;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+         stack= new Stack<>();
+         help = new Stack<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        while (!stack.isEmpty()){
+            help.push(stack.pop());
+        }
+        stack.push(x);
+        while (!help.isEmpty()){
+            stack.push(help.pop());
+        }
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        return stack.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        return stack.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack.empty();
+    }
 }
+
+
+
