6fcc3e7ecd13e2c788ceaf59890b34acdefca636
QponahuijiuY <1530201142@qq.com>
2020-03-11 22:54:33 +0800

    旋转字符串

---
+++ src/com/mutong/offer/MidNode.java
@@ -0,0 +1,10 @@
+package com.mutong.offer;
+
+/**
+ * @description:
+ * @Author: Mutong
+ * @Date: 2020-03-03 22:29
+ * @time_complexity: O()
+ */
+public class MidNode {
+}
---
+++ src/com/mutong/offer/ReverseLeftWords.java
@@ -0,0 +1,31 @@
+package com.mutong.offer;
+
+/**
+ * @description: 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入: s = "abcdefg", k = 2
+ * 输出: "cdefgab"
+ * 示例 2：
+ *
+ * 输入: s = "lrloseumgh", k = 6
+ * 输出: "umghlrlose"
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author: Mutong
+ * @Date: 2020-03-11 22:51
+ * @time_complexity: O()
+ */
+public class ReverseLeftWords {
+    public String reverseLeftWords(String s, int n) {
+        String str = "";
+        str = s.substring(n, s.length());
+        str +=s.substring(0,n);
+        return str;
+    }
+}
