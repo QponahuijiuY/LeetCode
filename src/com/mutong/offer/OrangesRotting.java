f50be4c507fbb367786271ceb5412feffb9a6caf
QponahuijiuY <1530201142@qq.com>
2020-03-15 00:25:17 +0800

    坏橘子

--- src/com/mutong/offer/OrangesRotting.java
+++ src/com/mutong/offer/OrangesRotting.java
@@ -1,10 +1,92 @@
 package com.mutong.offer;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
- * @description:
+ * @description: 在给定的网格中，每个单元格可以有以下三个值之一：
+ *
+ * 值 0 代表空单元格；
+ * 值 1 代表新鲜橘子；
+ * 值 2 代表腐烂的橘子。
+ * 每分钟，任何与腐烂的橘子（在 4 个正方向上）相邻的新鲜橘子都会腐烂。
+ *
+ * 返回直到单元格中没有新鲜橘子为止所必须经过的最小分钟数。如果不可能，返回 -1。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/rotting-oranges
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * @Author: Mutong
  * @Date: 2020-03-04 23:32
  * @time_complexity: O()
  */
 public class OrangesRotting {
+    public static int  orangesRotting(int[][] grid) {
+        if(grid == null || grid.length == 0) {
+            return 0;
+        }
+        int rows = grid.length;
+        int cols = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        int count_fresh = 0;
+        //Put the position of all rotten oranges in queue
+        //count the number of fresh oranges
+        for(int i = 0 ; i < rows ; i++) {
+            for(int j = 0 ; j < cols ; j++) {
+                if(grid[i][j] == 2) {
+                    queue.offer(new int[]{i , j});
+                }
+                else if(grid[i][j] == 1) {
+                    count_fresh++;
+                }
+            }
+        }
+        //if count of fresh oranges is zero --> return 0
+        if (count_fresh == 0) {
+            return 0;
+        }
+        int count = 0;
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        //bfs starting from initially rotten oranges
+        while(!queue.isEmpty()) {
+            ++count;
+            int size = queue.size();
+            for(int i = 0 ; i < size ; i++) {
+                int[] point = queue.poll();
+                for(int dir[] : dirs) {
+                    int x = point[0] + dir[0];
+                    int y = point[1] + dir[1];
+                    //if x or y is out of bound
+                    //or the orange at (x , y) is already rotten
+                    //or the cell at (x , y) is empty
+                    //we do nothing
+                    if(x < 0 || y < 0 || x >= rows || y >= cols || grid[x][y] == 0 || grid[x][y] == 2) {
+                        continue;
+                    }
+                    //mark the orange at (x , y) as rotten
+                    grid[x][y] = 2;
+                    //put the new rotten orange at (x , y) in queue
+                    queue.offer(new int[]{x , y});
+                    //decrease the count of fresh oranges by 1
+                    count_fresh--;
+                }
+            }
+        }
+        return count_fresh == 0 ? count-1 : -1;
+    }
+
+    public  static void main(String[] args) {
+        int[][] grid = new int[3][3];
+        grid[0][0] = 2;
+        grid[0][1] = 2;
+        grid[0][2] = 2;
+        grid[1][0] = 2;
+        grid[1][1] = 2;
+        grid[1][2] = 2;
+        grid[2][0] = 2;
+        grid[2][1] = 2;
+        grid[2][2] = 2;
+        int i = orangesRotting(grid);
+        System.out.println(i);
+    }
 }
