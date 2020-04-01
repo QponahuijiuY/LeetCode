e55180bce32a29f68be233a6228ade8af1f875e1
QponahuijiuY <1530201142@qq.com>
2020-04-01 21:24:40 +0800

    和为s的连续正数序列

--- src/com/mutong/offer/FindContinuousSequence.java
+++ src/com/mutong/offer/FindContinuousSequence.java
@@ -27,15 +27,16 @@ import java.util.List;
  * @time_complexity: O()
  */
 public class FindContinuousSequence {
+    public static void main(String[] args) {
+        FindContinuousSequence findContinuousSequence = new FindContinuousSequence();
+        System.out.println(findContinuousSequence.FindContinuousSequence(10000));
+    }
     public int[][] findContinuousSequence(int target) {
-
         int i = 1;
         int j = 1;
         int sum = 0;
         List<int[]> list = new ArrayList<>();
         while (i <= target / 2) {
-
-
             if (sum < target) {
                 sum += j;
                 j++;
@@ -53,6 +54,33 @@ public class FindContinuousSequence {
             }
         }
         return list.toArray(new int[list.size()][]);
+    }
 
+
+    public ArrayList<ArrayList<Integer> > FindContinuousSequence(int sum) {
+        //存放结果
+        ArrayList<ArrayList<Integer> > result = new ArrayList<>();
+        //两个起点，相当于动态窗口的两边，根据其窗口内的值的和来确定窗口的位置和大小
+        int plow = 1,phigh = 2;
+        while(phigh > plow){
+            //由于是连续的，差为1的一个序列，那么求和公式是(a0+an)*n/2
+            int cur = (phigh + plow) * (phigh - plow + 1) / 2;
+            //相等，那么就将窗口范围的所有数添加进结果集
+            if(cur == sum){
+                ArrayList<Integer> list = new ArrayList<>();
+                for(int i=plow;i<=phigh;i++){
+                    list.add(i);
+                }
+                result.add(list);
+                plow++;
+                //如果当前窗口内的值之和小于sum，那么右边窗口右移一下
+            }else if(cur < sum){
+                phigh++;
+            }else{
+                //如果当前窗口内的值之和大于sum，那么左边窗口右移一下
+                plow++;
+            }
+        }
+        return result;
     }
 }
