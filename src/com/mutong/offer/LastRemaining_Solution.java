a3450b20f940a0d36d8b721e5a8d1521c2630c37
QponahuijiuY <1530201142@qq.com>
2020-04-03 11:40:22 +0800

    孩子们的游戏

--- src/com/mutong/offer/LastRemaining_Solution.java
+++ src/com/mutong/offer/LastRemaining_Solution.java
@@ -7,4 +7,15 @@ package com.mutong.offer;
  * @time_complexity: O()
  */
 public class LastRemaining_Solution {
+    public static void main(String[] args) {
+        System.out.println(LastRemaining_Solution(10, 3));
+    }
+    public static int LastRemaining_Solution(int n, int m) {
+
+        if(n==0) return -1;
+        int s=0;
+        for(int i=2;i<=n;i++){s=(s+m)%i;}
+        return s;
+
+    }
 }
