b5f31b7d1137769f26c812b01f8665d1d8b9c35f
QponahuijiuY <1530201142@qq.com>
2020-05-02 00:25:11 +0800

    三角形路径的最小值

--- src/com/mutong/leetcode/Solution120.java
+++ src/com/mutong/leetcode/Solution120.java
@@ -1,10 +1,49 @@
 package com.mutong.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * @description:
+ * @description: 三角形路径的最小值
  * @Author: Mutong
  * @Date: 2020-04-30 22:15
  * @time_complexity: O()
  */
 public class Solution120 {
+    public static void main(String[] args) {
+
+    }
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int[] dp = new int[triangle.size()+1];
+        int size = triangle.size();
+        for (int i = size-1; i >= 0; i--) {
+            List<Integer> mini = new ArrayList<>();
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                  dp[j] = Math.min(dp[j] , dp[j+1]) +triangle.get(i).get(j);
+            }
+        }
+        return dp[0];
+
+    }
+    public int minimumTotal1(List<List<Integer>> triangle) {
+        int[] dp = new int[triangle.size()+1];
+        for (int i = triangle.size()-1; i >= 0; i--) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                //dp(i,j) = min( dp(i-1,j) dp(i-1,j+1) ) + nums[i][j]
+               dp[j]  = Math.min(dp[j],dp[j+1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0];
+    }
+    public int minimumTotal2(List<List<Integer>> triangle) {
+
+        int[][] dp = new int[triangle.size()+1][triangle.size()+1];
+        for (int i = triangle.size()-1; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                dp[i][j] = Math.min(dp[i+1][j],dp[i+1][j+1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0][0];
+    }
 }
---
+++ src/com/mutong/leetcode/Solution121.java
@@ -0,0 +1,10 @@
+package com.mutong.leetcode;
+
+/**
+ * @description:
+ * @Author: Mutong
+ * @Date: 2020-05-01 22:29
+ * @time_complexity: O()
+ */
+public class Solution121 {
+}
