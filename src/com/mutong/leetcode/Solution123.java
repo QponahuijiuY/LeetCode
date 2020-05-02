13acf3523c189b04d3133017329b5d23ef5179e1
QponahuijiuY <1530201142@qq.com>
2020-05-02 15:57:28 +0800

    买卖股票问题,最多2次

--- src/com/mutong/leetcode/Solution123.java
+++ src/com/mutong/leetcode/Solution123.java
@@ -7,4 +7,40 @@ package com.mutong.leetcode;
  * @time_complexity: O()
  */
 public class Solution123 {
+    public int maxProfit(int[] prices) {
+        if(prices.length<2){return 0;}
+        int k = 2;
+        int[][][] dp = new int[prices.length][k+1][2];
+        for (int i = 1; i <= k; i++) {
+            //定义初始值
+            dp[0][i][0]=0;
+            dp[0][i][1]=-prices[0];
+        }
+        for (int i = 1; i < prices.length; i++) {
+            for (int j = k; j >= 1; j--) {
+                dp[i][j][0] = Math.max(dp[i-1][j][0],dp[i-1][j][1] + prices[i]);
+                dp[i][j][1] = Math.max(dp[i-1][j][1],dp[i-1][j-1][0]-prices[i]);
+            }
+        }
+        return dp[prices.length-1][k][0];
+    }
+
+
+    public int maxProfit1(int[] prices) {
+        int k = 2;
+        int[][][] dp = new int[prices.length+1][k+1][2];
+        dp[0][1][0]=0;
+        dp[0][1][1]=-prices[0];
+        dp[0][2][0]=0;
+        dp[0][2][1]=-prices[0];
+        for (int i = 1; i < prices.length; i++) {
+                dp[i][2][0] = Math.max(dp[i-1][2][0],dp[i-1][2][1] + prices[i]);
+                dp[i][2][1] = Math.max(dp[i-1][2][1],dp[i-1][1][0] - prices[i]);
+                dp[i][1][0] = Math.max(dp[i-1][1][0],dp[i-1][1][1] + prices[i]);
+                dp[i][1][1] = Math.max(dp[i-1][1][1],-prices[i]);
+        }
+        return dp[prices.length-1][k][0];
+
+
+    }
 }
