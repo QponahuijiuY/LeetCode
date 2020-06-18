3b52d04d05665a3d81454d804792725d1661f780
QponahuijiuY <1530201142@qq.com>
2020-06-18 11:08:56 +0800

    从头到尾打印链表

---
+++ src/com/mutong/leetcode/ListNode.java
@@ -0,0 +1,9 @@
+package com.mutong.leetcode;
+
+/**
+ * @description:
+ * @Author: gengchen.jing@yoyi.com.cn
+ * @Date: 2020-06-18 11:01
+ */
+public class ListNode {
+}
--- src/com/mutong/leetcode/jianzhi6.java
+++ src/com/mutong/leetcode/jianzhi6.java
@@ -1,9 +1,29 @@
 package com.mutong.leetcode;
 
+
+
+import java.util.ArrayList;
+import java.util.Stack;
+
 /**
- * @description:
+ * @description: 从头到尾打印链表
  * @Author: gengchen.jing@yoyi.com.cn
  * @Date: 2020-06-18 10:55
  */
 public class jianzhi6 {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        Stack<ListNode> stack = new Stack<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        ListNode cur = listNode;
+        while (cur != null){
+            stack.push(cur);
+            cur = cur.next;
+        }
+        while (!stack.isEmpty()){
+            res.add(stack.pop().val);
+        }
+        return res;
+    }
 }
+
+
